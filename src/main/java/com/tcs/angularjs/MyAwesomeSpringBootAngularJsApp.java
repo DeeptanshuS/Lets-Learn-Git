78a08909db7659d6d5399491c5ad44c6891f548f
DeeptanshuS <107979678+DeeptanshuS@users.noreply.github.com>
2022-07-15 15:25:48 +0530

    Update MyAwesomeSpringBootAngularJsApp.java

--- src/main/java/com/tcs/angularjs/MyAwesomeSpringBootAngularJsApp.java
+++ src/main/java/com/tcs/angularjs/MyAwesomeSpringBootAngularJsApp.java
@@ -1,6 +1,6 @@
 package com.tcs.angularjs;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
